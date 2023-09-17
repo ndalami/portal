2e3ca3d9c32b916898ec8cfab821c21475631b3d
ndalami <bundala001@gmail.com>
2023-09-17 23:16:22 +0300

    botec to wibo

--- vacancy/settings.py
+++ vacancy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+__1-4ednc#28efo1+wqh)f_vn3(6vkym)+!ce6^v06da=spm^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.2.153']
 
 
 # Application definition
